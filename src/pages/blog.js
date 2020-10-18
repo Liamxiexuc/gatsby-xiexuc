029497778ebef6a0c22e11412bc2fac2e91ca0be
Xiexuc <liachenxiexu@gmail.com>
2020-10-18 19:57:44 +1100

    setup graghQL query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,36 @@
 import React from "react"
-
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>BlogPage,</h1>
-            <h2>Posts will show here</h2>
+            <ol>
+                {data.allMarkdownRemark.edges.map(edge => {
+                    return (
+                        <li>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
---
+++ src/posts/react.md
@@ -0,0 +1,12 @@
+---
+title: "React"
+date: "2020-10-18"
+---
+
+In this post you'll learn React.
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQL
+3. React

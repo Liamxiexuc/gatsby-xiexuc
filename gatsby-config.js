5498611b3a71dfa54f350b4083fc54ef6aee3c45
Xiexuc <liachenxiexu@gmail.com>
2020-10-28 21:29:58 +1100

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,19 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Xiexuc.xyz`,
+        short_name: `Xiexuc`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`
+      },
+    },
   ],
   pathPrefix: "/gatsby-xiexuc"
 }

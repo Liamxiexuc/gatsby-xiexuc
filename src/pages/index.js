7e47f92eaaf86f119d42a1eff86a3016eb5ca7d4
Xiexuc <liachenxiexu@gmail.com>
2020-10-28 22:42:54 +1100

    update cover image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Fire from '../assets/fire.mp4'
+import Img from "gatsby-image"
 import landingStyles from './index.module.scss'
 
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
       <section className={landingStyles.showcase}>
-        <div className={landingStyles.videoContainer}>
-          <video src={Fire} autoPlay muted loop></video>
-        </div>
+        <video src={Fire} autoPlay muted loop></video>
+        <Img alt='fire' className={landingStyles.coverImg} style={{position: 'absolute'}} fluid={data.file.childImageSharp.fluid}/>
         <div className={landingStyles.content}>
           <h1>Light On Your Camping Fire</h1>
           <h3>I'm Xiexu Chen, a full-stack developer living in Hobart.</h3>
@@ -24,5 +24,19 @@ const IndexPage = () => {
       </section>
   )
 }
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "fire.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 2000) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage;
